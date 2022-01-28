2040b99e49b00913405963b1e72719bef3bb815d
Maulik Khandelwal <kmaulik07@gmail.com>
2022-01-28 20:49:29 +0530

    loading text changed

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,7 +2,7 @@ function Alert({ message, color }) {
     return (
         <div>
             {message
-                ? <div className={`bg-${color} z-50 rounded-xl mb-4 py-3 px-4 text-xl text-bgrey font-bold`}>{message}</div>
+                ? message === "Loading..." ? <div className={`bg-${color} z-50 rounded-xl mb-4 py-3 px-4 text-xl text-white font-bold`}>{message}</div> : <div className={`bg-${color} z-50 rounded-xl mb-4 py-3 px-4 text-xl text-bgrey font-bold`}>{message}</div>
                 : <div></div>
             }
         </div>
